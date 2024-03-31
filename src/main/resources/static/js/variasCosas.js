5c4114450715dfa65efce79dea3a603d896d81d3
HP EliteBook 840 g7 <HP EliteBook 840 g7@SHW>
2024-03-31 13:34:44 -0600

    ya sirve los botones solo falta agregar las funcionalidades correctas y agregar las funcioens en categoria

--- src/main/resources/static/js/variasCosas.js
+++ src/main/resources/static/js/variasCosas.js
@@ -21,15 +21,5 @@ function addCard(formulario) {
     $("#resultsBlock").load(url);
 }
 
-const open= document.getElementById('open');
-const modal_container= document.getElementById('modal_fade');
-const close=document.getElementById('close');
 
-open.addEventListener('click', () => {
-   modal_container.classList.add('show');
-   alert('prueba');
-});
-close.addEventListener('click', () => {
-   modal_container.classList.remove('show');
-   
-});
+
